a27567348efc3efd215642aff711de76c939c328
Huy Bui <huybui@Huys-MacBook-Pro-2.local>
2025-08-23 22:23:20 -0500

    Solution

---
+++ 860.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        h_map = {5:0, 10:0, 20:0} #Tien thoi. Hash table
+        for b in bills:
+            if b == 5:
+                h_map[5] += 1
+            elif b == 10:
+                h_map[10] += 1
+                h_map[5] -= 1
+            elif b == 20:
+                h_map[20] += 1 
+                if h_map[10] > 0: #Greedy algorithm
+                    h_map[10] -= 1
+                    h_map[5] -= 1
+                else:
+                    h_map[5] -= 3                    
+            else:
+                print("Invalid")
+                return False  
+            if (h_map[5] < 0) or (h_map[10] < 0):
+                return False
+        return True
+        
+# Space Complexity: O(1) => 5, 10, 20
+# Time Complexity: O(n) => number of bills
