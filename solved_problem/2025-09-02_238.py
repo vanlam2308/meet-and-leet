8e9ad60b89b6ef426d16535df47af8c3ce1b15b1
Huy Bui <huybui@Huys-MBP-2.attlocal.net>
2025-09-02 22:15:22 -0500

    238

--- solved_problem/2025-09-02_238.py
+++ solved_problem/2025-09-02_238.py
@@ -1,18 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
         
-        left_prod, right_prod = [1], [1] #Cumprod left and right
-        for num in nums:
-            left_prod.append(left_prod[-1]*num)
-        for num in nums[::-1]:
-            right_prod.append(right_prod[-1]*num)
+        n = len(nums)
+        left_prod = [1] #Cumprod left and right
+        for i in range(n-1):
+            left_prod.append(left_prod[-1]*nums[i])
+
+        right_prod = [1]
+        for i in range(n-1):
+            right_prod.append(right_prod[-1]*nums[-1-i])
+
         right_prod = right_prod[::-1]
-        # print(left_prod, right_prod)
-        # [1,2,3,4]
-        # [1,1,2,6,24] Left cumprod
-        # [24,24,12,4,1] Right cumprod
-        # [24,12,8,6] 
+        print(left_prod, right_prod)
+        #   1, 2, 3, 4
+        #   ^  
+        # 1,1, 2, 6     #Left product
+        #     24,12,4,1 #Right product
+        #   24,12,8,6
         res = []
         for i in range(len(nums)):
-            res.append(left_prod[i] * right_prod[i+1])
+            res.append(left_prod[i] * right_prod[i])
         return res
